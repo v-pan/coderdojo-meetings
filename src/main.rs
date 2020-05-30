f4dee7b5444f39486166ea601274d5024604004f
paranoidcake <victorpanayotov@gmail.com>
2020-05-30 21:30:55 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use web_view::*;
+
+fn main() {
+    let html_content = include_str!("../dist/bundle.html");
+
+    web_view::builder()
+        .title("My Project")
+        .content(Content::Html(html_content))
+        .size(320, 480)
+        .resizable(false)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
+}
