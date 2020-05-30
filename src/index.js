db944dae7379a09a2a387e1b9f77a9cce4f4787c
paranoidcake <victorpanayotov@gmail.com>
2020-05-30 22:04:30 +0100

    Added binding from Javascript to Rust

--- src/index.js
+++ src/index.js
@@ -8,11 +8,17 @@ class HelloMessage extends React.Component {
             <Header/>
             <div className="container">
                 <h1>Hello {this.props.name}</h1>
+                <button onClick={() => { rust.test() }}>Click me to print from Rust!</button>
             </div>
         </div>
     }
 }
 
+let rust = {
+    invoke: (arg) => { window.external.invoke(JSON.stringify(arg)) },
+    test: () => { rust.invoke({cmd: 'test', text: 'This is a test'}) }
+}
+
 let App = document.getElementById("app");
 
-ReactDOM.render(<HelloMessage name="Yomi" />, App);
+ReactDOM.render(<HelloMessage name="React" />, App);
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+extern crate serde_json;
+extern crate serde_derive;
+extern crate web_view;
+
 use web_view::*;
+use serde_derive::*;
 
 fn main() {
     let html_content = include_str!("../dist/bundle.html");
@@ -10,7 +15,23 @@ fn main() {
         .resizable(false)
         .debug(true)
         .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
+        .invoke_handler(|_webview, arg| {
+            use Cmd::*;
+
+            match serde_json::from_str(arg).unwrap() {
+                Init => (),
+                Test {text} => println!("{}", text)
+            }
+
+            Ok(())
+        })
         .run()
         .unwrap();
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "cmd", rename_all = "camelCase")]
+pub enum Cmd {
+    Init,
+    Test {text: String}
 }
